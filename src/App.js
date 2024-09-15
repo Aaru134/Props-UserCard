c13e0d866846837c64f96fcca71667a31c3c54cc
Arti Digambar Pawar <ap193368@gmail.com>
2024-09-15 04:33:29 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import users from "./userDb";
+import UserCard from "./UserCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    return (
+  
+      <div className="app">
+  
+        {users.map(({ id, name, designation, isAdmin, photo }) => (
+  
+          <UserCard
+  
+            key={id}
+  
+            name={name}
+  
+            designation={designation}
+  
+            isAdmin={isAdmin}
+  
+            photo={photo}
+  
+          />
+  
+        ))}
+  
+      </div>
+  
+    );
+  
+  };
 
 export default App;
